c5e82ec45573b8010c11a39254796d86702945d2
OuardaHolberton <12985@holbertonstudents.com>
2026-06-13 12:37:39 +0200

    Fix task 1 order by states.id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    )
     for row in cur.fetchall():
         print(row)
     cur.close()
