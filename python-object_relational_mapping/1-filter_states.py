9a4e4c6079aba462953cd271c35f68a6b5ff3213
OuardaHolberton <12985@holbertonstudents.com>
2026-06-13 12:43:40 +0200

    Add task 1: filter states starting with upper N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Script that lists all states starting with N from a database"""
+"""
+This module provides a script that connects to a MySQL database
+and lists all states starting with an uppercase 'N' from the table
+'states', sorted in ascending order by states.id.
+"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE LEFT(name, 1) = 'N' ORDER BY id ASC"
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
     )
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+                "ORDER BY states.id ASC")
+
     for row in cur.fetchall():
         print(row)
+
     cur.close()
     db.close()
